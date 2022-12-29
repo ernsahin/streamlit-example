8aed57d077fb72daaeabd7ac5778e0a00961e475
ernsahin <93529206+ernsahin@users.noreply.github.com>
2022-12-29 22:19:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
-with st.echo(code_location='below'):
+ 
     df = pd.read_json("https://node-api.flipsidecrypto.com/api/v2/queries/446958b3-a694-4bb5-98a0-41d556a96c5d/data/latest")
 # df_1 = pd.DataFrame(df["SALES"],columns=df["PROJECT_NAMEE"])
 alt.Chart(df).mark_bar().encode(
